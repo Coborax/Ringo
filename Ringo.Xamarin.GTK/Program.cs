46166c967bbf1f1a4c5baead114f80cbd1fae2b1
Mikkel L. Mouridsen <mikkelmouridsen@me.com>
2020-08-01 17:40:09 +0200

    Initial Xamarin commit (I renamed a couple of folders)

---
+++ Ringo.Xamarin.GTK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace Ringo.Xamarin.GTK
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("りんご GTK Test");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+using Ringo.Xamarin;
+
+namespace Test
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("りんご GTK Test");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
